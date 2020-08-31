d58c2dc6bad69e92348fdbb5ca172657fc82961d
Heiko Becker <hbecker@mpi-sws.org>
2020-08-31 19:24:48 +0200

    Incorporate beginning of feedback from Ivan

--- setup_hol4.sh
+++ setup_hol4.sh
@@ -5,16 +5,12 @@ BASEDIR=/opt/test/
 
 cd $BASEDIR
 
-# Make sure necessary packages are installed:
-#apt-get update && \
-#apt-get install -y build-essential aspcud m4 wget unzip git curl ant ruby openjdk-8-jdk
-
 # Install polyml from git
 git clone https://github.com/polyml/polyml.git polyml && \
     cd polyml && \
     ./configure --prefix=/usr && make && make compiler && make install
 
-# Install HOL3
+# Install HOL4
 cd $BASEDIR && \
     git clone https://github.com/HOL-Theorem-Prover/HOL HOL && \
     cd HOL && git checkout master &&\
@@ -25,5 +21,6 @@ export HOLDIR=$BASEDIR/HOL/
 cd $BASEDIR && \
     git clone https://gitlab.mpi-sws.org/AVA/lassie.git Lassie &&\
     cd Lassie/sempre && ./pull-dependencies core interactive && ant core interactive
+    cd $BASEDIR/Lassie/src && $HOLDIR/bin/Holmake
 
 echo "Please add the following lines to your shell configuration file:\n export HOLDIR=$BASEDIR/HOL/ \nexport LASSIEDIR=$BASEDIR/Lassie/"
